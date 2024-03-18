1467a620d13b9a63f7876ad7ee7a7bb1cdf09348
alicagdascolak <alicagdas.colak@vektortelekom.com>
2024-03-19 00:14:21 +0300

    app header created

---
+++ App.style.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  app: {
+    padding: 20,
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  header: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  body: {
+    flex: 5,
+    backgroundColor: "#F9F9F9",
+  },
+  footer: {
+    height: 70,
+    backgroundColor: "#F9F9F9",
+  },
+});
